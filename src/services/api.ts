5a6dd62361dcec22b24072fb379f1ec65a24bb31
PedroSantos <pedrohenry.viana@gmail.com>
2021-05-24 11:36:37 -0300

    chore(services): add new domain for API

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,5 @@
 import axios from 'axios';
-/*
-export const api = axios.create({
-    baseURL: 'https://podcastr-pedrohenry-santos.vercel.app/api'
-});
-*/
 
 export const api = axios.create({
-    baseURL: 'https://teste-api-poscaster.vercel.app/api'
+    baseURL: 'https://podcastr-pedrohenry-santos.vercel.app/api'
 });
